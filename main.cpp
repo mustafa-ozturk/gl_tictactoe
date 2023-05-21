ea6d15441b09d6155e66c3b3af4bdab14d0ff75d
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-21 09:45:57 -0400

    add draw_line

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ struct Square {
 };
 
 std::map<int, Square> create_squares();
+void draw_line(int x_start, int x_end, int y_start, int y_end);
 
 int main()
 {
@@ -131,4 +132,41 @@ std::map<int, Square> create_squares()
     eight.y_start = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
     eight.y_start = SCREEN_HEIGHT;
     squares.insert({8, eight});
+}
+
+void draw_line(int x_start, int x_end, int y_start, int y_end)
+{
+    /*
+     * A --- B
+     * */
+    std::array<int, 8> vertices{
+            x_start, y_start,   // A
+            y_start, y_end,     // B
+    };
+
+    std::array<GLuint, 2> indices{
+            0, 1,
+    };
+
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(int), vertices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_INT, GL_FALSE, 2 * sizeof(GLuint), (const void*) 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(int), indices.data(), GL_STATIC_DRAW);
+
+    glDrawElements(GL_LINES, indices.size(), GL_UNSIGNED_INT, nullptr);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
