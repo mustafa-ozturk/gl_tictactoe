4313eed08a9a4913faa92077f06dd830ecbb09ed
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-21 10:34:12 -0400

    fix create_squares

--- main.cpp
+++ main.cpp
@@ -132,63 +132,63 @@ std::map<int, Square> create_squares()
     zero.x_start = 0;
     zero.x_end = SCREEN_WIDTH / 3;
     zero.y_start = 0;
-    zero.y_start = SCREEN_HEIGHT / 3;
+    zero.y_end = SCREEN_HEIGHT / 3;
     squares.insert({0, zero});
 
     Square one{};
     one.x_start = SCREEN_WIDTH / 3;
     one.x_end = SCREEN_WIDTH - SCREEN_WIDTH / 3;
     one.y_start = 0;
-    one.y_start = SCREEN_HEIGHT / 3;
+    one.y_end = SCREEN_HEIGHT / 3;
     squares.insert({1, one});
 
     Square two{};
     two.x_start = SCREEN_WIDTH - SCREEN_WIDTH / 3;
     two.x_end = SCREEN_WIDTH;
     two.y_start = 0;
-    two.y_start = SCREEN_HEIGHT / 3;
+    two.y_end = SCREEN_HEIGHT / 3;
     squares.insert({2, two});
 
     Square three{};
     three.x_start = 0;
     three.x_end = SCREEN_WIDTH / 3;
     three.y_start = SCREEN_HEIGHT / 3;
-    three.y_start = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
+    three.y_end = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
     squares.insert({3, three});
 
     Square four{};
     four.x_start = SCREEN_WIDTH / 3;
     four.x_end = SCREEN_WIDTH - SCREEN_WIDTH / 3;
     four.y_start = SCREEN_HEIGHT / 3;
-    four.y_start = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
+    four.y_end = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
     squares.insert({4, four});
 
     Square five{};
     five.x_start = SCREEN_WIDTH - SCREEN_WIDTH / 3;
     five.x_end = SCREEN_WIDTH;
     five.y_start = SCREEN_HEIGHT / 3;
-    five.y_start = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
+    five.y_end = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
     squares.insert({5, five});
 
     Square six{};
     six.x_start = 0;
     six.x_end = SCREEN_WIDTH / 3;
     six.y_start = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
-    six.y_start = SCREEN_HEIGHT;
+    six.y_end = SCREEN_HEIGHT;
     squares.insert({6, six});
 
     Square seven{};
     seven.x_start = SCREEN_WIDTH / 3;
     seven.x_end = SCREEN_WIDTH - SCREEN_WIDTH / 3;
     seven.y_start = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
-    seven.y_start = SCREEN_HEIGHT;
+    seven.y_end = SCREEN_HEIGHT;
     squares.insert({7, seven});
 
     Square eight{};
     eight.x_start = SCREEN_WIDTH - SCREEN_WIDTH / 3;
     eight.x_end = SCREEN_WIDTH;
     eight.y_start = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
-    eight.y_start = SCREEN_HEIGHT;
+    eight.y_end = SCREEN_HEIGHT;
     squares.insert({8, eight});
 
     return squares;
