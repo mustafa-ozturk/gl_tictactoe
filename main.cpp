ece34bdf1afb57eb3ce485b9562ca48e6c491bd8
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-21 08:28:49 -0400

    add create_squares function

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ struct Square {
     int y_end;
 };
 
+std::map<int, Square> create_squares();
+
 int main()
 {
     if (!glfwInit()) return -1;
@@ -38,6 +40,8 @@ int main()
 
     glbinding::initialize(glfwGetProcAddress);
 
+    std::map<int, Square> squares = create_squares();
+
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
@@ -49,4 +53,79 @@ int main()
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
+}
+
+std::map<int, Square> create_squares()
+{
+    /*
+     * 6 | 7 | 8
+     * --|---|--
+     * 3 | 4 | 5
+     * --|---|--
+     * 0 | 1 | 2
+     * */
+    std::map<int, Square> squares = {};
+
+    Square zero;
+    zero.x_begin = 0;
+    zero.x_end = SCREEN_WIDTH / 3;
+    zero.y_begin = 0;
+    zero.y_begin = SCREEN_HEIGHT / 3;
+    squares.insert({0, zero});
+
+    Square one;
+    one.x_begin = SCREEN_WIDTH / 3;
+    one.x_end = SCREEN_WIDTH - SCREEN_WIDTH / 3;
+    one.y_begin = 0;
+    one.y_begin = SCREEN_HEIGHT / 3;
+    squares.insert({1, one});
+
+    Square two;
+    two.x_begin = SCREEN_WIDTH - SCREEN_WIDTH / 3;
+    two.x_end = SCREEN_WIDTH;
+    two.y_begin = 0;
+    two.y_begin = SCREEN_HEIGHT / 3;
+    squares.insert({2, two});
+
+    Square three;
+    three.x_begin = 0;
+    three.x_end = SCREEN_WIDTH / 3;
+    three.y_begin = SCREEN_HEIGHT / 3;
+    three.y_begin = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
+    squares.insert({3, three});
+
+    Square four;
+    four.x_begin = SCREEN_WIDTH / 3;
+    four.x_end = SCREEN_WIDTH - SCREEN_WIDTH / 3;
+    four.y_begin = SCREEN_HEIGHT / 3;
+    four.y_begin = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
+    squares.insert({4, four});
+
+    Square five;
+    five.x_begin = SCREEN_WIDTH - SCREEN_WIDTH / 3;
+    five.x_end = SCREEN_WIDTH;
+    five.y_begin = SCREEN_HEIGHT / 3;
+    five.y_begin = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
+    squares.insert({5, five});
+
+    Square six;
+    six.x_begin = 0;
+    six.x_end = SCREEN_WIDTH / 3;
+    six.y_begin = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
+    six.y_begin = SCREEN_HEIGHT;
+    squares.insert({6, six});
+
+    Square seven;
+    seven.x_begin = SCREEN_WIDTH / 3;
+    seven.x_end = SCREEN_WIDTH - SCREEN_WIDTH / 3;
+    seven.y_begin = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
+    seven.y_begin = SCREEN_HEIGHT;
+    squares.insert({7, seven});
+
+    Square eight;
+    eight.x_begin = SCREEN_WIDTH - SCREEN_WIDTH / 3;
+    eight.x_end = SCREEN_WIDTH;
+    eight.y_begin = SCREEN_HEIGHT - SCREEN_HEIGHT / 3;
+    eight.y_begin = SCREEN_HEIGHT;
+    squares.insert({8, eight});
 }
