c9173324969395e862143664f0e53614da2712ec
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-21 06:18:47 -0400

    initial window and setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <glbinding/glbinding.h>
+#include <glbinding/gl/gl.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "gl_textrenderer/gl_textrenderer.h"
+#include "gl_gridlines/gl_gridlines.h"
+
+using namespace gl;
+
+const unsigned int SCREEN_WIDTH = 500;
+const unsigned int SCREEN_HEIGHT = 500;
 
 int main()
 {
+    if (!glfwInit()) return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "gl_tictactoe", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glbinding::initialize(glfwGetProcAddress);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
