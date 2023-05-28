41342c19a73b23a79d75a2a34d3fe3e1f08b7868
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-28 10:13:07 -0400

    cleanup

--- main.cpp
+++ main.cpp
@@ -54,8 +54,6 @@ enum GAME_STATE
 
 std::map<int, Square> create_squares();
 
-void draw_line(int x_start, int x_end, int y_start, int y_end);
-
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 
 Player next_player(Player current_player, int& turn);
@@ -481,46 +479,6 @@ std::map<int, Square> create_squares()
     squares.insert({8, eight});
 
     return squares;
-}
-
-void draw_line(int x_start, int x_end, int y_start, int y_end)
-{
-    /*
-     * A --- B
-     * */
-    std::array<int, 8> vertices{
-            x_start, y_start,   // A
-            x_end, y_end,     // B
-    };
-
-    std::array<unsigned int, 2> indices{
-            0, 1,
-    };
-
-    unsigned int VAO, VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(int),
-                 vertices.data(), GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 2, GL_INT, GL_FALSE, 2 * sizeof(int),
-                          (const void*) 0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
-                 indices.data(), GL_STATIC_DRAW);
-
-    glDrawElements(GL_LINES, indices.size(), GL_UNSIGNED_INT, nullptr);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
