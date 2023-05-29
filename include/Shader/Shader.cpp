347b75691059f2a8c3b24968fde0d441fe1ca38c
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-29 17:07:32 -0400

    use vec4 instead of vec3 for color in shader

--- include/Shader/Shader.cpp
+++ include/Shader/Shader.cpp
@@ -18,11 +18,11 @@ Shader::Shader()
         #version 330 core
         out vec4 FragColor;
 
-        uniform vec3 color;
+        uniform vec4 color;
 
         void main()
         {
-            FragColor = vec4(color.xyz, 1.0f);
+            FragColor = vec4(color);
         }
     )";
 
